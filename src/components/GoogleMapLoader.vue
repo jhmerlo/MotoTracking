f131b5930a54fbcd447887ce94f5f684aac4c1ba
José <jose@vixtecnologia.com.br>
2021-03-04 23:24:10 -0300

    added reverse geocoding

--- src/components/GoogleMapLoader.vue
+++ src/components/GoogleMapLoader.vue
@@ -4,31 +4,28 @@
 
 <script>
 import gmapsInit from '../utils/gmaps'
-const location = {
-  position: {
-    lat: '-20.256172',
-    lng: '-40.271153'
-  }
-}
 export default {
   name: 'App',
-  async mounted () {
+  props: {
+    location: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  async created () {
     try {
       const google = await gmapsInit()
-      const geocoder = new google.maps.Geocoder()
-      const map = new google.maps.Map(this.$el)
-
-      geocoder.geocode({ address: 'Austria' }, (results, status) => {
-        if (status !== 'OK' || !results[0]) {
-          throw new Error(status)
-        }
-
-        map.setCenter(results[0].geometry.location)
-        map.fitBounds(results[0].geometry.viewport)
-
-        const marker = new google.maps.Marker({ location, map })
-        console.log(marker)
+      const mapOptions = {
+        center: new google.maps.LatLng(this.location.lat, this.location.lng),
+        zoom: 14,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      }
+      const map = new google.maps.Map(this.$el, mapOptions)
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.location.lat, this.location.lng),
+        map
       })
+      console.log(marker)
     } catch (error) {
       console.error(error)
     }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,31 +1,49 @@
 <template>
-  <q-page class="bg-grey-1" padding>
-    <q-card class="q-ma-sm bg-grey-9 q-mt-md shadow-4">
-      <q-card-section class="row text-center">
-        <div class="text-caption q-mt-md col-12 text-grey-1">
-          Última Localização
-        </div>
-        <div class="text-h6 col-12 text-primary">
-          Jardim Camburi
-        </div>
-      </q-card-section>
-    </q-card>
+  <q-page class="bg-grey-4">
+    <div class="bg-grey-10 q-pa-md row text-center shadow-4">
+      <div class="col-12 text-center">
+        <q-icon name="location_on" color="primary" size="40px" />
+      </div>
+      <div class="text-caption q-mt-sm col-12 text-grey-5">
+        Última Localização
+      </div>
+      <div class="text-h6 col-12 text-primary">
+        {{ actualLocalization }}
+      </div>
+    </div>
     <div class="col-12 q-mt-md">
-      <GoogleMapLoader class="q-ma-sm" style="height: 500px" />
+      <google-map-loader
+        v-if="location"
+        class="q-ma-sm"
+        style="height: 68vh"
+        :location="location"
+      />
     </div>
   </q-page>
 </template>
 
 <script>
 import GoogleMapLoader from 'components/GoogleMapLoader'
+import axios from 'axios'
 export default {
   name: 'PageIndex',
   components: {
     GoogleMapLoader
   },
+  data: () => ({
+    location: {
+      lat: -20.174181,
+      lng: -40.273449
+    },
+    apiKey: 'AIzaSyAxiknv-6jGy94ydJQ3UoKnxNGfRBWg2nY',
+    actualLocalization: ''
+  }),
   async created () {
     const user = await this.$store.getters['auth/isLogged']
     if (!user) return this.$router.push({ name: 'login' })
+    const { data } = await axios.get((`https://maps.googleapis.com/maps/api/geocode/json?key=${this.apiKey}&latlng=` + this.location.lat + ',' + this.location.lng))
+    const localization = data.plus_code.compound_code
+    this.actualLocalization = localization.substr(localization.indexOf(' ') + 1)
   }
 }
 </script>
